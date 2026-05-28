906b9d5a2bb30ca202388843ab565e0245dc5760
surendevops75 <surendevops75@gmail.com>
2026-05-28 13:16:45 +0530

    github-runner

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "daws86s.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z01894583A5LLRAMEST3A"
+  description = "description"
+}
